642ddc488cb661e9af1e2cd70a273cd94c16ac26
ivan2 <ivan20050924@gmail.com>
2025-04-23 12:37:19 +0200

    Repo GitHubMiner Integration Project AISS (CommentService Complete, ProjectService & IssueService Failed)

---
+++ src/test/java/aiss/githubminer/service/IssueServiceTest.java
@@ -0,0 +1,39 @@
+package aiss.githubminer.service;
+
+import aiss.githubminer.model.Issue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+public class IssueServiceTest {
+
+    @Autowired
+    IssueService service;
+
+    @Test
+    @DisplayName("List of Issues")
+    void getIssuesRepo() {
+        String owner = "spring-projects";
+        String repo = "spring-framework";
+        List<Issue> issues = null;
+        issues = service.getIssues(owner, repo);
+        assertFalse(issues.isEmpty(), "The list of issues is empty!");
+        System.out.println(issues);
+    }
+
+    @Test
+    @DisplayName("List of an Issue")
+    void getIssue() {
+        String owner = "spring-projects";
+        String repo = "spring-framework";
+        String number = "34800";
+        Issue issue = null;
+        issue = service.getIssueId(owner, repo, number);
+        assertFalse(issue == null, "The issue is null!");
+        System.out.println(issue);
+    }
+}
---
+++ src/test/java/aiss/githubminer/service/ProjectServiceTest.java
@@ -0,0 +1,30 @@
+package aiss.githubminer.service;
+
+import aiss.githubminer.model.Project;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ProjectServiceTest {
+
+    @Autowired
+    ProjectService projectService;
+
+    @Test
+    @DisplayName("Get Projects of an organization")
+    void getProjectDetail() {
+        String owner = "github";
+        Integer number = 5;
+        List<Project> projects = null;
+        projects = projectService.getProjects(owner, number);
+        assertNotNull(projects);
+        System.out.println(projects);
+    }
+
+}
